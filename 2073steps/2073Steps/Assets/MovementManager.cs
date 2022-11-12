6c49eba2412c1481663025e7c100ebd97cc0dd52
João Vitor Barroso <joaov98@gmail.com>
2022-11-12 12:44:24 -0300

    Completa Movement Manager
    Coloca o movement manager para habilitar e desabilitar conforme o personagem atual.

--- 2073steps/2073Steps/Assets/MovementManager.cs
+++ 2073steps/2073Steps/Assets/MovementManager.cs
@@ -11,10 +11,67 @@ public class MovementManager : MonoBehaviour
 
     void Start()
     {
+        current_character = GameManager.current_character;
+        enableCharacterScript(current_character);
     }
 
     void Update()
     {
+        
         current_character = GameManager.current_character;
+        if (getCurrentActiveScript() != current_character)
+        {
+            enableCharacterScript(current_character);
+            disableCharacterScript(getCurrentActiveScript());
+        }
+    }
+
+    Character getCurrentActiveScript()
+    {
+        if (maratonistaScript.enabled)
+        {
+            return Character.Maratonista;
+        }
+        else if (cientistaScript.enabled)
+        {
+            return Character.Cientista;
+        }    
+        else
+        {
+            return Character.Fantasma;
+        }
+    }
+
+    void enableCharacterScript(Character character)
+    {
+        if (character == Character.Maratonista)
+        {
+            maratonistaScript.enabled = true;
+        }
+        else if (character == Character.Cientista)
+        {
+            cientistaScript.enabled = true;
+        }
+        else
+         {
+            fantasmaScript.enabled = true;
+        }
+
+    }
+
+    void disableCharacterScript(Character character)
+    {
+        if (character == Character.Maratonista)
+        {
+            maratonistaScript.enabled = false;
+        }
+        else if (character == Character.Cientista)
+        {
+            cientistaScript.enabled = false;
+        }
+        else
+        {
+            fantasmaScript.enabled = false;
+        }
     }
 }
